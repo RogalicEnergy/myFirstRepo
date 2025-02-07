aa37a180146c64a6ec65138baf31d92bdfe0ade9
Anastasia Mikhailova <fasechka_99@mail.ru>
2025-02-07 21:12:24 +0300

    Коррекция задания к уроку №6

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function gameBot() {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
 
   function ListOfQuestions() {
-    const userRequest = +prompt('Угадай число от 1 до 100');
+    let userRequest = prompt('Угадай число от 1 до 100');
   
     if (userRequest === null) {
       alert('Игра окончена');
@@ -21,12 +21,12 @@ function gameBot() {
       alert('Загаданное число меньше');
       ListOfQuestions();
       } else if (userRequest < randomNumber) {
-      alert('Загаданное число больше');
-      ListOfQuestions();
+        alert('Загаданное число больше');
+        ListOfQuestions();
       } else {
         alert('Поздравляю, Вы угадали!!!')
       }
-      
+
   }
   
   ListOfQuestions()
