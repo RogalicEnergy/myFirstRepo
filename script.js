6c8b42071c8ac2969c41a9bd07d15b72550003ba
Anastasia Mikhailova <fasechka_99@mail.ru>
2025-02-07 20:48:06 +0300

    Выполнено задание к уроку №6

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-const a = 1
-let b = "вторая переменная"
+'use srtict';
+
+function gameBot() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+  function ListOfQuestions() {
+    const userRequest = +prompt('Угадай число от 1 до 100');
+  
+    if (userRequest === null) {
+      alert('Игра окончена');
+      return;
+    }
+  
+    if (isNaN(userRequest)) {
+      alert('Введи число!');
+      ListOfQuestions();
+      return;
+    }
+  
+    if (userRequest > randomNumber) {
+      alert('Загаданное число меньше');
+      ListOfQuestions();
+      } else if (userRequest < randomNumber) {
+      alert('Загаданное число больше');
+      ListOfQuestions();
+      } else {
+        alert('Поздравляю, Вы угадали!!!')
+      }
+      
+  }
+  
+  ListOfQuestions()
+
+}
+
+gameBot()
+
